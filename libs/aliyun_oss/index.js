e0253f45cdcea0d020b9ce3dd5ad8fb1858dee90
TabbyCute <66464361+TabbyCute@users.noreply.github.com>
2022-09-27 11:23:31 +0800

    Update index.js

--- libs/aliyun_oss/index.js
+++ libs/aliyun_oss/index.js
@@ -6,8 +6,8 @@ class Config {
         success_action_status:200,                                          //上传成功后的网络响应码
         OssAddress:`http://wh-fy.oss-cn-hangzhou.aliyuncs.com/`,   // OSS地址
         bucketSrc:"fy_talk_file/",                                             // 需要存入的oss路径
-        AccessKeySecret:'ijVL6QvEbdIbAoe4xHBKma5wCeNNz9',               //
-        OSSAccessKeyId:'LTAI5t7dF1GY84Xa6JW3wxUw',                     //
+        AccessKeySecret:'*',               //
+        OSSAccessKeyId:'*',                     //
         timeout:87600,                                          //这个是上传文件时Policy和signature的有效时间小时
         conditions:[["content-length-range",0,1024*3000]],       //规则          //生成policy如果没有可能报400
     }
